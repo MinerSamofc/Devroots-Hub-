ccc0cf04402caaba3b2fc7f76b27bdb8235327c0
MinerSamofc <samuelcarmooficial@gmail.com>
2025-11-23 02:38:05 -0400

    Add files via upload

---
+++ SplashActivity.java
@@ -0,0 +1,32 @@
+package com.devroots.devrootshub;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SplashActivity extends AppCompatActivity {
+
+    private static final int SPLASH_DURATION = 1800; // ms
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        ImageView logo = findViewById(R.id.splashLogo);
+        Animation anim = AnimationUtils.loadAnimation(this, R.anim.splash_fade_in);
+        logo.startAnimation(anim);
+
+        new Handler().postDelayed(() -> {
+            Intent intent = new Intent(SplashActivity.this, MainActivity.class);
+            startActivity(intent);
+            overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out);
+            finish();
+        }, SPLASH_DURATION);
+    }
+}
